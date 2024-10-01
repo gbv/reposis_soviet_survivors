7df3ad4e21aa7d96a2035617b7fb6ff89c4b4a56
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2024-10-01 08:20:42 +0200

    QUED-133-MCR-Viewer-Ansicht per default mit Transkription öffnen

---
+++ src/main/java/de/gbv/reposis/sovietsurvivors/SovietSurvivorsViewerConfigurationStrategy.java
@@ -0,0 +1,18 @@
+package de.gbv.reposis.sovietsurvivors;
+
+import org.mycore.mir.viewer.MIRViewerConfigurationStrategy;
+import org.mycore.viewer.configuration.MCRViewerConfiguration;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class SovietSurvivorsViewerConfigurationStrategy extends MIRViewerConfigurationStrategy {
+
+    @Override
+    public MCRViewerConfiguration get(HttpServletRequest request) {
+        MCRViewerConfiguration defaultConfig = super.get(request);
+
+        defaultConfig.setProperty("text.showOnStart", "transcription");
+
+        return defaultConfig;
+    }
+}
